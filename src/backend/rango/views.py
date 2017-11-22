5a2cf20ca648d9c7fb0da9c1405b08e32e833502
davi-dev <ieronim@mail.com>
2017-11-22 10:07:15 +0200

    configured settings.py so that we can use django templates, modified views.py by adding a dictionary that mapped the boldmessage value to the template in te index.html, modified the return statement with the render function, added a templates folder in the rango app and and created the index.html page, added in the django template in the file

--- src/backend/rango/views.py
+++ src/backend/rango/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return HttpResponse('Welcome to the website. <a href="about">Go To About Page.</a>')
+	context_dict = {'boldmessage': 'Some bold message here...'}
+	return render(request, 'rango/index.html', context= context_dict)
+
 
 def about(request):
 	return HttpResponse('Here is the about page. <a href="/rango">Go To Home Page.</a>')
