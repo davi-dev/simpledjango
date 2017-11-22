a43f75485f4801b3924f5920964f5ccd9b687d83
davi-dev <ieronim@mail.com>
2017-11-22 09:37:47 +0200

    created about view, mapped it in the rango/urls.py, added a link to the rango page and back

--- src/backend/rango/views.py
+++ src/backend/rango/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Welcome to the website.")
+	return HttpResponse('Welcome to the website. <a href="about">Go To About Page.</a>')
+
+def about(request):
+	return HttpResponse('Here is the about page. <a href="/rango">Go To Home Page.</a>')
